ae66eb62d6038dcc0078113ab899ec1f1c2f416c
Dmitriy Nikitin <nikitin.dv27@gmail.com>
2023-08-31 20:43:46 +0300

    Fix raw sql

--- lib/Doctrine/Migrations/Query/PreparableQuery.php
+++ lib/Doctrine/Migrations/Query/PreparableQuery.php
@@ -6,7 +6,7 @@ class PreparableQuery extends Query
 {
     public function __construct(string $statement, array $parameters = [], array $types = [])
     {
-        if(strpos( $statement,';')!==false){
+        if(substr_count( $statement,';')>1){
             throw new \ValueError('Preparable statement must contain single statement');
         }
         parent::__construct($statement, $parameters, $types);
